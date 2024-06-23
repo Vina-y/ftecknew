61a7f60486135ac65028883aa72936fd86acafaa
vinay kumar singh <vinay.kumar@gigacars.com>
2024-06-23 21:11:40 +0530

    l

--- ftech_app/settings.py
+++ ftech_app/settings.py
@@ -157,8 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ftech/static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
+STATICFILES_DIRS = os.path.join(BASE_DIR, "ftech/static"),
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static")
 
 
 # Default primary key field type
