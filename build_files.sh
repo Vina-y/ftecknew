a995a33e99ab4794a0e72c2448e76e20032d043f
vinay kumar singh <vinay.kumar@gigacars.com>
2024-06-23 20:09:56 +0530

    sf

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py makemigrations --noinput
-python3 manage.py makemigrations ftech_app --noinput
-python3 manage.py migrate --noinput
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
+python manage.py makemigrations --noinput
+python manage.py makemigrations ftech_app --noinput
+python manage.py migrate --noinput
 echo "BUILD END"
