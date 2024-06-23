7331afe19b32f5e4160b038426e900ea31022a47
vinay kumar singh <vinay.kumar@gigacars.com>
2024-06-23 20:32:13 +0530

    sdf

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,8 @@
 echo "BUILD START"
-python -m pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py makemigrations ftech_app --noinput
-python3.9 manage.py migrate --noinput
+python -m pip install -r requirements.txt
+echo "requirements installed"
+python manage.py collectstatic --noinput --clear
+python manage.py makemigrations --noinput
+python manage.py makemigrations ftech_app --noinput
+python manage.py migrate --noinput
 echo "BUILD END"
