ffc791b2964d3f94b617b4ee500bd58596ae09d9
vinay kumar singh <vinay.kumar@gigacars.com>
2024-06-23 20:55:09 +0530

    gh

--- ftech_app/settings.py
+++ ftech_app/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
 
 
 # Default primary key field type
