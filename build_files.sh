d2d010f423a9db870e13024fa015c7cd120c0707
vinay kumar singh <vinay.kumar@gigacars.com>
2024-06-23 21:47:22 +0530

    sad

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,13 @@
-echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-echo "requirements installed"
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py makemigrations ftech_app --noinput
-python3.9 manage.py migrate --noinput
-echo "BUILD END"
+# echo "BUILD START"
+# python3.9 -m pip install -r requirements.txt
+# echo "requirements installed"
+# python3.9 manage.py collectstatic --noinput --clear
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py makemigrations ftech_app --noinput
+# python3.9 manage.py migrate --noinput
+# echo "BUILD END"
+
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
